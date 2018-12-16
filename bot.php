bdec135626368cb8a27fa42b5aabdd75a80569b9
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2018-12-16 08:56:49 +0800

    Latest code on chatbot

--- bot.php
+++ bot.php
@@ -1,3 +1,4 @@
+
 <?php
 require 'library/twitteroauth/autoload.php';
 require 'vendor/autoload.php';
@@ -15,11 +16,11 @@ $access_tokensecret = "4YhheEa8hZbRxIcJ6awnlhIsTlxmiwPN5D2DEVmBR68Eh";
 $connection = new TwitterOAuth($consumer, $consumersecret, $access_token, $access_tokensecret);
 $content = $connection->get("account/verify_credentials");
 function beliefmedia_hashtags($string) {
- 	preg_match_all('/#(\w+)/', $string, $matches);
-  	foreach ($matches[1] as $match) {
-    	$keywords[] = '#'.$match;
-  	} 
- 	return (array) $keywords;
+  preg_match_all('/#(\w+)/', $string, $matches);
+    foreach ($matches[1] as $match) {
+      $keywords[] = '#'.$match;
+    } 
+  return (array) $keywords;
 }
 
 function callAPI($accessToken,$response,$input){
@@ -34,8 +35,8 @@ function callAPI($accessToken,$response,$input){
 }
 
 if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-	echo $_REQUEST['hub_challenge'];
-	exit;
+  echo $_REQUEST['hub_challenge'];
+  exit;
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
@@ -44,8 +45,8 @@ $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 $command = strtolower($messageText);
 
-$reminder = 'reminder';
-$history = 'history';
+$reminder = 'reminder'.$senderId;
+$history = 'history'.$senderId;
 $historyFile = fopen($history, 'a');
 $data = file($history);
 
@@ -56,10 +57,12 @@ $record = true;
 $response_tweet = false;
 
 if($lastCommand == "remind"){
-	$messageText = "what:" . $messageText;
-	$answer = "When do you want to be reminded?";  
+  $messageText = "what:" . $messageText;
+  $answer = "When do you want to be reminded?";  
 }
 elseif(substr($lastCommand, 0, 5) === "what:") {
+
+  date_default_timezone_set('Asia/Manila');
   if (($timestamp = strtotime($command)) === false) {
     $answer = "Reminder was not added. Date was invalid.";
   } else {     
@@ -68,121 +71,121 @@ elseif(substr($lastCommand, 0, 5) === "what:") {
     $cronformat = $min." ".date('G j n * ', $timestamp - $offset);  
     $logformat = date('Y\-m\-d H:i', $timestamp);
     $msgformat = date('d F Y \a\t h:i A', $timestamp); 
-  	$remindMsg = substr($lastMsg, 5);
+    $remindMsg = substr($lastMsg, 5);
   
-  	$reminderFile = fopen($reminder, 'a');
-  	$data = file($reminderFile);
-  	fwrite($reminderFile, $logformat . ' - ' . $remindMsg . "\n");
+    $reminderFile = fopen($reminder, 'a');
+    $data = file($reminderFile);
+    fwrite($reminderFile, $logformat . ' - ' . $remindMsg . "\n");
     fclose($reminderFile); 
-  		
-  	$output = shell_exec('crontab -l');
-  	$cronCommand = '/usr/bin/php /var/www/html/chatbot/reminderScript.php '.$senderId.' '.$accessToken.' '.urlencode($remindMsg).' > /dev/null 2>/dev/null &';
-  	file_put_contents('crontab.txt', $output.$cronformat.$cronCommand.PHP_EOL);     
-  	shell_exec('crontab crontab.txt');
+      
+    $output = shell_exec('crontab -l');
+    $cronCommand = '/usr/bin/php /var/www/html/chatbot/reminderScript.php '.$senderId.' '.$accessToken.' '.urlencode($remindMsg).' > /dev/null 2>/dev/null &';
+    file_put_contents('crontab.txt', $output.$cronformat.$cronCommand.PHP_EOL);     
+    shell_exec('crontab crontab.txt');
  
-  	$answer = "Okay! I will remind you about this on ".$msgformat.".";
+    $answer = "Okay! I will remind you about this on ".$msgformat.".";
     }  
 }
 else{
-  	if($command == "hi" or $command == "hello") {
-		$answer = "Welcome to Remindeer!";
-  	}
-  	elseif ($command == "remind") {
-		$answer = "What is the reminder about?";
-  	}
-  	elseif ($command == "list") {
-		$answer = file_get_contents($reminder);
-		$remindList = "";
-		
-		$handle = fopen($reminder, "r");
-		$lineNumber = 1;
-		if ($handle) {
-			while (($line = fgets($handle)) !== false) {        
-				if($line !== "")
-				{
-					$remindList = $remindList . "[ " . $lineNumber . " ] " . $line;
-					$lineNumber++;
-				}
-			}    
-			fclose($handle);
-		} 	
-		$answer = $remindList;
-  	}
-	elseif ($command == "last") {
-		$answer = $lastMsg;
-	}
+    if($command == "hi" or $command == "hello") {
+    $answer = "Welcome to Remindeer!";
+    }
+    elseif ($command == "remind") {
+    $answer = "What is the reminder about?";
+    }
+    elseif ($command == "list") {
+    $answer = file_get_contents($reminder);
+    $remindList = "";
+    
+    $handle = fopen($reminder, "r");
+    $lineNumber = 1;
+    if ($handle) {
+      while (($line = fgets($handle)) !== false) {        
+        if($line !== "")
+        {
+          $remindList = $remindList . "[ " . $lineNumber . " ] " . $line;
+          $lineNumber++;
+        }
+      }    
+      fclose($handle);
+    }   
+    $answer = $remindList;
+    }
+  elseif ($command == "last") {
+    $answer = $lastMsg;
+  }
   elseif(substr($command, 0, 8) === "listen #") {
-		$get_hashtag =  array_unique(beliefmedia_hashtags($command));
-		$hashtag = $get_hashtag[0];
-		$answer =  "You are now listening to ".$hashtag." tweets.";
-		
-		$response = [
+    $get_hashtag =  array_unique(beliefmedia_hashtags($command));
+    $hashtag = $get_hashtag[0];
+    $answer =  "You are now listening to ".$hashtag." tweets.";
+    
+    $response = [
             'recipient' => [ 'id' => $senderId ],
             'message' => [ 'text' => $answer ]
         ];
         callAPI($accessToken,$response,$input);
-		
-		$tweets = $connection->get("search/tweets", [
-			"q" => $hashtag,
-			"include_entities"=>true,
-			"count"=> 10
-		]);
-		
-		if($tweets){
-			$attachment = null;
-			
-			foreach ($tweets as $key => $tweet) {
-				if($key == 'statuses'){
-					$elements = [];
-					$counter = 0;
-					foreach ($tweet as $keyT => $t) {
-						$item_url = "https://twitter.com/".$t->user->screen_name."/status/".$t->id_str;
-						$elements[$counter]['title'] = $t->user->name.' (@'.$t->user->screen_name.')';
-						$elements[$counter]['item_url'] = $item_url;
-						$elements[$counter]['image_url'] = str_replace('_normal', '', $t->user->profile_image_url_https);
-						$elements[$counter]['subtitle'] = $t->text;
-						$elements[$counter]['buttons'][] = array('type' => "web_url",'url' => $item_url,'title' => 'View Tweet');
-						$counter++;
-					}
-				}
-			}
-			if($elements){
-				$answer = "A tweet matched ".$hashtag.":";
-				$response = [
+    
+    $tweets = $connection->get("search/tweets", [
+      "q" => $hashtag,
+      "include_entities"=>true,
+      "count"=> 10
+    ]);
+    
+    if($tweets){
+      $attachment = null;
+      
+      foreach ($tweets as $key => $tweet) {
+        if($key == 'statuses'){
+          $elements = [];
+          $counter = 0;
+          foreach ($tweet as $keyT => $t) {
+            $item_url = "https://twitter.com/".$t->user->screen_name."/status/".$t->id_str;
+            $elements[$counter]['title'] = $t->user->name.' (@'.$t->user->screen_name.')';
+            $elements[$counter]['item_url'] = $item_url;
+            $elements[$counter]['image_url'] = str_replace('_normal', '', $t->user->profile_image_url_https);
+            $elements[$counter]['subtitle'] = $t->text;
+            $elements[$counter]['buttons'][] = array('type' => "web_url",'url' => $item_url,'title' => 'View Tweet');
+            $counter++;
+          }
+        }
+      }
+      if($elements){
+        $answer = "A tweet matched ".$hashtag.":";
+        $response = [
                     'recipient' => [ 'id' => $senderId ],
                     'message' => [ 'text' => $answer ]
                 ];
                 callAPI($accessToken,$response,$input);
                 
-				$attachment = ["attachment"=>[
-					"type"=>"template",
-					"payload"=>[
-					 "template_type"=> "generic",
-					 "image_aspect_ratio"=> "horizontal",
-					"elements"=> $elements
-					]
-				]];
-				$response_tweet = true;
-			}else{
-			    $answer = "No tweets found.";
-    			$response = [
+        $attachment = ["attachment"=>[
+          "type"=>"template",
+          "payload"=>[
+           "template_type"=> "generic",
+           "image_aspect_ratio"=> "horizontal",
+          "elements"=> $elements
+          ]
+        ]];
+        $response_tweet = true;
+      }else{
+          $answer = "No tweets found.";
+          $response = [
                     'recipient' => [ 'id' => $senderId ],
                     'message' => [ 'text' => $answer ]
                 ];
                 callAPI($accessToken,$response,$input);
-			}
-		}else{
-			$answer = "No tweets found.";
-			$response = [
+      }
+    }else{
+      $answer = "No tweets found.";
+      $response = [
                 'recipient' => [ 'id' => $senderId ],
                 'message' => [ 'text' => $answer ]
             ];
             callAPI($accessToken,$response,$input);
-		}
-	} elseif(substr($command, 0, 8) === "cancel #") {	
+    }
+  } elseif(substr($command, 0, 8) === "cancel #") { 
     $get_hashtag =  array_unique(beliefmedia_hashtags($command)); 
-    $hashtag = $get_hashtag[0];		
-    $answer =  "You are no longer listening to ".implode(', ',$get_hashtags)." tweets.";	
+    $hashtag = $get_hashtag[0];   
+    $answer =  "You are no longer listening to ".implode(', ',$get_hashtags)." tweets.";  
   } elseif (substr($command, 0, 6) === "cancel") {
     $num = substr($command, 6);
     $handle = fopen($reminder, "r");
@@ -214,7 +217,7 @@ else{
     $remindtime = strtotime(substr($selectedLine, 0, 16));
     $remindmsg = preg_replace( "/\r|\n/", "", substr($selectedLine, 19));
     
-    $answer = 'Canceled reminder about "'.$remindmsg.'" on '.date("d F Y h:i A.", $remindtime);
+    $answer = 'Cancelled reminder about "'.$remindmsg.'" on '.date("d F Y h:i A.", $remindtime);
     
   } elseif ($command == "crontab") {
     $answer = shell_exec('crontab -l');
@@ -240,8 +243,8 @@ else{
 
 if($record == true)
 {
-	fwrite($historyFile, "\n". $messageText);
-	fclose($historyFile);
+  fwrite($historyFile, "\n". $messageText);
+  fclose($historyFile);
 }
 
 
